3eb0311481135fbcff05d93728078f7bdbaaf972
theMightyT <halvas_trevor@halvas.ca>
2021-10-14 15:07:42 -0400

    removed conflict text

--- includes/functions.php
+++ includes/functions.php
@@ -2,15 +2,6 @@
     // store the processed results in a variable
     $result = array();
 
-<<<<<<< HEAD
-    function getProfData($conn, $prof) {
-        if (is_null($prof)) {
-            $query = "SELECT * FROM profs";
-        } else {
-            $query = "SELECT * FROM profs WHERE id='". $prof ."'";
-        }
-
-=======
     // if a user passes an ID via a query string (?id=1)
     // then we should retrieve the row of data that matches and pass it back to the app
     function getOneProf($conn, $prof) {        
@@ -34,7 +25,6 @@
         $query = "SELECT * FROM profs";
 
         // this is the database result -> the raw data that SQL gives us
->>>>>>> 70158474b0c7d92724dc9af36adc3cf36f8f2a9d
         $runQuery = $conn->query($query);    
 
         // process our DB result and make something we can use with AJAX
--- includes/index.php
+++ includes/index.php
@@ -2,17 +2,8 @@
     require("connect.php");
     require("functions.php");
 
-<<<<<<< HEAD
-    // get our data (one row or many) from our DB
-    // if we pass in the id param, use it. else set it to null
-    $id = isset($_GET["id"]) ? $_GET["id"] : null;
-    
-    // and then send that data back to our JS Fetch API
-    // $result = getProfData($pdo, $id);
-    echo json_encode(getProfData($pdo, $id));
-=======
-   // filter the incoming basic request and call the appropriate function
-   // (these live in functions.php)
+    // filter the incoming request
+
    if (isset($_GET["id"])) {
        // this is the id that the user is passing to the script (1, 2, 3 etc) => ?id=1
        // it referes to the row of data that we want to retrieve from the DB
@@ -26,4 +17,3 @@
 
    // send the database result (our data) back to the javascript file
    echo json_encode($result);
->>>>>>> 70158474b0c7d92724dc9af36adc3cf36f8f2a9d
