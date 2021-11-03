48140a466e19da16c123f0318f38ab381bb84aa8
theMightyT <halvas_trevor@halvas.ca>
2021-11-03 17:33:36 -0400

    lastest with Vue components and data

--- js/main.js
+++ js/main.js
@@ -6,16 +6,30 @@ import TheThumbNail from "./components/TheThumbNail.js";
     const myVue = new Vue({
         created: function() {
             // go fetch the portfolio data here
-            getData(null, (data) => this.portfolioData = data )
+            // make it available in the Vue instance
+            getData(null, (data) => this.portfolioData = data );
         },
 
         data: {
             // this is storing the database info from our fetch call
             portfolioData: [],
-            message: "hello from Vue"
+            message: "hello from Vue",
+            isVisible: false,
+            currentPortfolioItem = {}
         },
 
         methods: {
+            popLightBox(item) {
+                //debugger;
+                //let portHeader = document.querySelector(".port_title");
+
+                //portHeader.textContent = item.name;
+                this.currentPortfolioItem = item;
+            },
+
+            toggleLightbox() {
+                this.isVisible = true;
+            }
 
         },
 
